299ec971ad3ce47c3bb1db9f3317d052d18afa61
NishantxST <frey0xd@gmail.com>
2022-10-09 14:09:09 +0530

    Basic Math

--- src/print.rs
+++ src/print.rs
@@ -3,5 +3,26 @@ pub fn run(){
     // print 
     println!("Hello there twitter = print.rs");
     println!("Number :-{}", 42 );
+
+    // formatting 
+    println!("{} is from {}", "Frey", "New Delhi");
+    
+    // Positional Arguments
+    println!("{0} is from {1} and {0} likes to {2}",
+             "Pinchu", "India", "Code");
+
+    // Name Arguments
+    println!("{name} likes to play {game}",
+             name = "Frey", game = "RDR2");
+
+    // Placeholder 
+    println!("Binary: {:b} Hex {:x} Octal {:o}", 
+             10, 6018, 8);
+    // Placeholder Debug trail
+    println!("{:?}", (12, true, "frey"));
+
+    // Baic Math
+    println!("{} + {}", 12+12, 42-32);
+
 }
 
