028ba2d8b16de2b129402a678f334e8219170796
NishantxST <frey0xd@gmail.com>
2022-10-12 20:23:39 +0530

    Advance Strings

--- src/strings.rs
+++ src/strings.rs
@@ -25,6 +25,20 @@ pub fn run(){
 // Contains 
     println!("Contains 'NxsT' ? {}", hello.contains("Nxst"));
     println!("Contains 'NxST' ? {}", hello.contains("NxSt"));
+    // replace 
+    println!("Replace: {}", hello.replace("NxST", "Frey"));
+    //loop
+    for word in hello.split_whitespace() {
+        println!("{}",word);
+    }
+    // create string with cpacity
+    let mut s = String::with_capacity(10);
+    s.push('a');
+    s.push('b');
+    // assertion test
+    assert_eq!(2, s.len());
+    assert_eq!(10, s.capacity());
+    println!("{}",s);
     println!("{}",hello);
     
 }
